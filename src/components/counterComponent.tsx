321ba361d9ad4e2e288c9798d6088a3615d85e71
Ruben Karlsson <neburgordon@gmail.com>
2021-02-10 07:02:08 +0100

    added list

--- src/components/counterComponent.tsx
+++ src/components/counterComponent.tsx
@@ -4,20 +4,29 @@ import { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 1,
-        tags: ["tag1", "tag2", "tag3"]
+        tags: ["tag1", "tag2", "tag3", "tag4"]
     };
 
     render() {
         return (
-            <div className="row" id="counter">
-                <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-                <button className="btn btn-dark">Increment</button>
-            </div >
+            <div className="container">
+                <div className="row my-3" id="counter">
+                    <div className="col">
+                        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+                        <button className="btn btn-light btn-sm">Increment</button>
+                    </div>
+                </div >
+
+                <ul className="row my-3" id="itemsList">
+                    {this.state.tags.map(tag => <li className="col-sm" key={tag}>{tag}</li>)}
+                </ul>
+
+            </div>
         );
     }
 
     getBadgeClass() {
-        let classes = "col badge m-2 badge-";
+        let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
     }
