636934be948b0e86f0715db4aa6cad123d08f193
Ruben Karlsson <neburgordon@gmail.com>
2021-02-16 19:23:19 +0100

    another commit

--- src/components/navComponent.tsx
+++ src/components/navComponent.tsx
@@ -10,25 +10,10 @@ export const NavComponent = ({ totalProducts }: NavComponentProps) => {
       <div className="container-fluid">
         <span className="navbar-brand">Navbar</span>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <span className="nav-link active" aria-current="page">
-              Home
-            </span>
-            <span className="nav-link">Features</span>
-            <span className="nav-link">Pricing</span>
-          </div>
+        <div className="navbar-nav">
+          <span className="nav-link">Home</span>
+          <span className="nav-link">Features</span>
+          <span className="nav-link">Pricing</span>
         </div>
         <div className="navbar-nav">
           <span className="nav-item" style={{ color: "whitesmoke" }}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./custom.scss";
 
 ReactDOM.render(
   <React.StrictMode>
